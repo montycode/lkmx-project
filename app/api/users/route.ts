768ca7943dbae59832c99866bbdce411512f3517
Omar Montoya <alfredo.montoya@cesunbc.edu.mx>
2025-04-30 01:25:32 -0700

    Created Initial Users Endpoints

---
+++ app/api/users/route.ts
@@ -0,0 +1,35 @@
+import { createUser, getAllUsers } from "@/services/userService";
+
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const users = await getAllUsers();
+    return NextResponse.json(users, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return NextResponse.json(
+      { error: "Error fetching users" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const { name, email } = await req.json();
+
+    if (!name || !email) {
+      return NextResponse.json(
+        { error: "Name and email are required" },
+        { status: 400 }
+      );
+    }
+
+    const user = await createUser(name, email);
+    return NextResponse.json(user, { status: 201 });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return NextResponse.json({ error: "Error creating user" }, { status: 500 });
+  }
+}
